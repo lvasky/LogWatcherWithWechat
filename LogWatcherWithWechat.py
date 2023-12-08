16d3580a8de010d05a0fdb1a452dfb2087b09511
lvshuocong <lvshuocong@sansi.com>
2023-12-08 19:16:22 +0800

    (Fix): 超时时间开放选择;增加更多的注释

--- LogWatcherWithWechat.py
+++ LogWatcherWithWechat.py
@@ -27,7 +27,7 @@ def alert_user():
     custom_font = Font(family="Helvetica", size=12, weight="bold")
     Label(msg_window, text="\n!!! 5s后开始发送微信消息!!!\n  如果不需要,请关闭Windows命令窗口 \n", font=custom_font).pack()
     
-    root.after(6000, root.destroy)
+    root.after(4000, root.destroy)
     root.mainloop()
 
 # Function to send a message
@@ -76,6 +76,7 @@ def send_msg(friend, contents , app_type="wechat"):
     for msg in contents:
         send(msg)
         time.sleep(0.1)
+    print("发送完成")
 
 # Function to check the last lines of a log file
 def check_last_lines_of_log(log_file_path, contents, check_strs, n=10):
@@ -87,11 +88,13 @@ def check_last_lines_of_log(log_file_path, contents, check_strs, n=10):
             if check_str in line:
                 contents.append("".join(last_lines))
                 contents.append("任务完成")
+                print("$ 检测到关键词: " + check_str + ",\n任务完成...")
                 return True, last_lines
     return False, last_lines
 
 # Function to continuously check a log file
-def continuously_check_log(log_file_path, contents, check_strs, interval=5, n=20):
+def continuously_check_log(log_file_path, contents, check_strs, interval=20,timeout_cnt=12,  n=20):
+    print("$ 开始持续侦测中,如果log文件" + str(timeout_cnt * interval) + "s没有更新,\n判定结束...")
     pre_last_lines = None
     cnt = 0
     while not stop_event.is_set():
@@ -100,9 +103,10 @@ def continuously_check_log(log_file_path, contents, check_strs, interval=5, n=20
             cnt += 1
         else:
             cnt = 0
-        if cnt > 5:
+        if cnt > timeout_cnt:
             contents.append("".join(last_lines))
             contents.append("任务失败")
+            print("$ 检测到log文件" + str(timeout_cnt * interval) + "s没有更新,判定任务失败...")
             break
         pre_last_lines = last_lines
         if found:
@@ -114,9 +118,12 @@ def select_file():
     root = tk.Tk()
     root.attributes("-topmost", True)
     root.withdraw()
-
+    print("$ 选择要监控的log文件")
+    time.sleep(1)
     file_path = filedialog.askopenfilename()
     root.destroy()
+    print("* 收到的log_path: "+ file_path)
+    print("-------------------------------------------------------------")
     return file_path
 
 # Function to prompt user input
@@ -125,6 +132,10 @@ def say_hello(check_dic , apptype_dic):
     print("-------------------------------------------------------------")
     print("$             hello , welcome to use this program !      ")
     print("$ 这个程序帮助您在检测fpga软件中的log文件,在编译完成后用微信提醒您.")
+    print("& 提示: enter键可以跳过任意需要输入的地方")
+    print("-------------------------------------------------------------")
+    print("* 请确保你的微信电脑版已经登录了.")
+    print("* 请确保你的fpga软件已经开始运行了.")
     print("-------------------------------------------------------------")
     print("$ 默认采用以下检测关键词:")
     for key, value in check_dic.items():
@@ -143,16 +154,13 @@ def say_hello(check_dic , apptype_dic):
     for check_str in check_strs:
         print("* " + check_str)
     print("-------------------------------------------------------------")
-    print("* 请确保你的微信电脑版已经登录了.")
-    print("* 请确保你的fpga软件已经开始运行了.")
-    print("-------------------------------------------------------------")
 
     friend_name = input("$ 请输入微信好友，想把消息发给谁，如果不输入，默认为'文件传输助手':")
     if not friend_name:
         friend_name = "文件传输助手"
     print("The friend's name is: " + friend_name)
     print("-------------------------------------------------------------")
-    print("$ 请选择要使用的软件:")
+    print("$ 可供选择的软件列表:")
     for key, value in apptype_dic.items():
         print("* " + key + " : " + value)
     key_input = input("$ 请选择要使用的软件:")
@@ -161,18 +169,33 @@ def say_hello(check_dic , apptype_dic):
     if key_input not in set(apptype_dic.keys()):
         key_input = "0"
         
-    print(apptype_dic[key_input])
+    print("最终选择的软件是:" + apptype_dic[key_input])
     print("-------------------------------------------------------------")
-    return friend_name, check_strs, apptype_dic[key_input]
+    print("$ 选择查询间隔时间(20s)和文件不更新后判定失败的检测次数(12次)")
+    interval_sec = input("$ 请输入查询间隔时间():")
+    timeout_cnt =  input("$ 请输入判定失败的所需检测次数():")
+    if interval_sec == "":
+        interval_sec = 20
+    else :
+        interval_sec = int(interval_sec)
+    
+    if timeout_cnt == "":
+        timeout_cnt = 12
+    else:
+        timeout_cnt = int(timeout_cnt)
+
+    print("查询间隔时间" + str(interval_sec) + "s" + "; 文件不更新后判定失败的检测次数" + str(timeout_cnt) + "次")
+    print("-------------------------------------------------------------")
+    return friend_name, check_strs, apptype_dic[key_input], interval_sec, timeout_cnt
 
 # Main function
 if __name__ == '__main__':
     contents = []
     default_check_dic = {"安路软件关键词": "Generate bits file", "vivado软件关键词": "Generate bitstream successfully"}
     default_apptype_dic = {"0": "wechat", "1": "wechat_work"}
-    friend_name, check_strs, app_type = say_hello(default_check_dic, default_apptype_dic)
+    friend_name, check_strs, app_type, interval_sec, timeout_cnt = say_hello(default_check_dic, default_apptype_dic)
     log_file_path = select_file()
-    continuously_check_log_thread = threading.Thread(target=continuously_check_log, args=(log_file_path, contents, check_strs, 1, 20))
+    continuously_check_log_thread = threading.Thread(target=continuously_check_log, args=(log_file_path, contents, check_strs, interval_sec, timeout_cnt , 20))
     continuously_check_log_thread.start()
     continuously_check_log_thread.join()  # Wait for log checking thread to complete
     send_msg(friend_name, contents, app_type)
